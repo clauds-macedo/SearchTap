c177a5e1ac04aae6bb8c2c37f83926229134cf7b
clauds-macedo <yanalogic@gmail.com>
2022-02-07 08:55:16 -0300

    added on project -> tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  important: true,
+  // Active dark mode on class basis
+  darkMode: "class",
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+  purge: {
+    content: ["./pages/**/*.tsx", "./components/**/*.tsx", "./src/**/*.{js,jsx,ts,tsx}",],
+    // These options are passed through directly to PurgeCSS
+  },
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        check: "url('/icons/check.svg')",
+        landscape: "url('/images/landscape/2.jpg')",
+      }),screens: {
+        'sm': '300px',
+        'md': '768px',
+        'lg': '1024px',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
+    },
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  },
+};
