e49b2bda60abdfb73166897e9be49b5f3a6de66c
clauds-macedo <yanalogic@gmail.com>
2022-02-10 08:30:32 -0300

    changed header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Dropdown from './Dropdown'
 import Toggle from './Toggle';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
+
+  const location = useLocation().pathname
+
   return (
-      <div className='flex w-100 justify-center items-center mt-6'>
+      <div className='flex w-100 justify-center items-center mt-6 sm:ml-2'>
           <div className='sm:flex w-11/12 justify-center p-1
           '>
                 <div className='flex justify-around gap-3.5 items-center'>
-                    <button><a>Suporte</a></button>
-                    <button><a>Logar</a></button>
+                    {location !== '/' ?
+                        <Link to="/"><button><a className='md:text-2xl'>SearchTap</a></button></Link>
+                    : null }
                     <Dropdown/>
                     <Toggle/>
                 </div>
