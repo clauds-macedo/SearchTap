8b5188f8feed5a0920c385760514c7fe9376c968
clauds-macedo <yanalogic@gmail.com>
2022-02-10 08:33:30 -0300

    added headers

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
+import env from '../env.json'
 
 const api = axios.create({
-    baseURL: "https://google-search3.p.rapidapi.com/api/v1"
+    baseURL: "https://google-search3.p.rapidapi.com/api/v1",
+    headers: {
+        'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+        'x-rapidapi-key': env.RAPID_API_KEY
+    }
 })
 
 export default api;
