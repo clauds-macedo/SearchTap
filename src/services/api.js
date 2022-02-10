00ac12153258ef36f9ec47e5eb33e5901d6961f2
Claudemir Macêdo <67011538+clauds-macedo@users.noreply.github.com>
2022-02-10 18:44:08 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
-import env from '../env.json'
 
 const api = axios.create({
     baseURL: "https://google-search3.p.rapidapi.com/api/v1",
     headers: {
         'x-rapidapi-host': 'google-search3.p.rapidapi.com',
-        'x-rapidapi-key': env.RAPID_API_KEY
+        'x-rapidapi-key': "6d2ce0374amsh1db72df4501bf49p1db917jsn1ccab734b647"
     }
 })
 
