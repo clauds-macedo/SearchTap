5588545ee66698bb13a713e170ffa4a3595721bf
clauds-macedo <yanalogic@gmail.com>
2022-02-10 08:29:14 -0300

    saving language by context

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,20 +1,22 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useState } from "react";
+import { Fragment, useContext, useState } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 import { ReactCountryFlag } from "react-country-flag";
+import { AppContext } from "../context/context";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function Dropdown() {
-  const [lang, setLang] = useState("Portuguese");
+  
+  const { lang, saveLang } = useContext(AppContext)
 
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-gray-200 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none">
+        <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-gray-200 sm:text-sm md:text-2xl font-medium text-gray-700 hover:bg-gray-50 focus:outline-none">
           {lang}
           <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
         </Menu.Button>
@@ -29,16 +31,13 @@ export default function Dropdown() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none bg-gray-100 ">
+        <Menu.Items className="origin-top-right absolute right-0 mt-2 sm:w-36 md:w-56 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none bg-gray-100 ">
           <div className="py-1">
             <Menu.Item>
-              {({ active }) => (
+              {() => (
                 <button
-                  className={classNames(
-                    active ? "bg-gray-100 text-gray-900 gap-3.5" : "text-gray-700",
-                    "block px-4 py-2 text-sm"
-                  )}
-                  onClick={() => setLang("Portuguese")}
+                  className="text-gray-700 block px-4 py-2 sm:text-sm md:text-2xl"
+                  onClick={() => saveLang("Portuguese")}
                 >
                   Portuguese
                   <ReactCountryFlag
@@ -52,13 +51,10 @@ export default function Dropdown() {
               )}
             </Menu.Item>
             <Menu.Item>
-              {({ active }) => (
+              {() => (
                 <button
-                  className={classNames(
-                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                    "block px-4 py-2 text-sm"
-                  )}
-                  onClick={() => setLang("English")}
+                  className="text-gray-700 block px-4 py-2 sm:text-sm md:text-2xl"
+                  onClick={() => saveLang("English")}
                 >
                   English
                   <ReactCountryFlag
