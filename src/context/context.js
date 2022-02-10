4e01701a6c510ddafd81c728c17d01fefc91397a
clauds-macedo <yanalogic@gmail.com>
2022-02-10 08:31:05 -0300

    context for darkTheme, search & language

--- src/context/context.js
+++ src/context/context.js
@@ -1,4 +1,35 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
-const AppContext = createContext()
-export default AppContext;
+export const AppContext = createContext()
+
+const AppProvider = ({ children }) => {
+    
+    const [darkTheme, setDarkTheme] = useState(false);
+    const [searchText, setSearchText] = useState("")
+    const [lang, setLang] = useState("Portuguese")
+
+    const saveSearchText = async (searchText) => {
+        setSearchText(searchText)
+    }
+
+    const saveLang = (newLang) => {
+        setLang(newLang)
+    }
+
+    return(
+        <AppContext.Provider
+        value={{
+            darkTheme,
+            setDarkTheme,
+            searchText,
+            saveSearchText,
+            lang, 
+            saveLang
+        }}
+        >
+            {children}
+        </AppContext.Provider>
+    )
+} 
+
+export default AppProvider;
