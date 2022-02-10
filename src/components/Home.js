54d53a1357ad91de864ba31c3d66ea722241d381
clauds-macedo <yanalogic@gmail.com>
2022-02-10 08:35:42 -0300

    done -> Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,18 @@
 import React from "react";
-import { FiSearch } from "react-icons/fi";
 import Header from "./Header";
+import Searchbar from "./Searchbar";
 
 const Home = () => {
+
   return (
     <>
       <div className="flex flex-col h-screen">
-      <Header />
-        <div className="flex justify-center items-center space-x-5 flex-col p-5 h-screen">
+        <Header />
+        <div className="flex justify-center items-center space-x-5 flex-col p-5 mb-10 h-screen">
           <h1 className="sm:text-4xl md:text-6xl lg:text-8xl mb-8 text-center">
             Search<span>Tap</span>
           </h1>
-          <div className="flex flex-row items-center w-full justify-center gap-x-5">
-            <input className="rounded-lg w-md w-6/12 border shadow-sm outline-none p-1 text-black hover:shadow-lg h-12" />
-            <button>
-              <FiSearch size={32} />
-            </button>
-          </div>
+        <Searchbar/>
         </div>
       </div>
     </>
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,10 +1,9 @@
 import React, { useContext } from "react";
-import AppContext from "../context/context";
+import { AppContext } from "../context/context";
 
 export default function Toggle() {
+  const { darkTheme, setDarkTheme } = useContext(AppContext);
 
-    const { darkTheme, setDarkTheme } = useContext(AppContext)
-    console.log(darkTheme)
   return (
     <div>
       <div className="mb-3 mt-2.5">
@@ -15,10 +14,10 @@ export default function Toggle() {
             id="Blue"
             className="checked:bg-gray-900 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-amber-400 border-4 appearance-none cursor-pointer
             "
-            onClick={() => 
-                setTimeout(() => {
-                    setDarkTheme(!darkTheme)
-                }, 200)
+            onClick={() =>
+              setTimeout(() => {
+                setDarkTheme(!darkTheme);
+              }, 200)
             }
           />
           <label
