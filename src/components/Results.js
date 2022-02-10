1dc28b566aa0f9d9a67df145158edd1e0703c453
clauds-macedo <yanalogic@gmail.com>
2022-02-10 11:20:00 -0300

    finished results page

--- src/components/Results.js
+++ src/components/Results.js
@@ -6,67 +6,79 @@ import { useLocation } from "react-router-dom";
 import Searchbar from "./Searchbar";
 import { Loading } from "./Loading";
 import { NavLink } from "react-router-dom";
-import { FiCamera, FiSearch } from "react-icons/fi";
+import { FiCamera, FiSearch, FiFileText, FiYoutube } from "react-icons/fi";
+import ReactPlayer from "react-player";
 
 const Results = () => {
   const { searchText, lang } = useContext(AppContext);
   const [searchData, setSearchData] = useState([]);
   const [imageData, setImageData] = useState([]);
+  const [newsData, setNewsData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const currentLocation = useLocation().pathname;
   const [numberOfImages, setNumberOfImages] = useState(4);
-  const navLinkTranslation = lang === "Portuguese" ? (
-    {
-      "all":"Todos",
-      "images":"Imagens",
-      "news":"Notícias",
-      "videos":"Vídeos"
-    }
-  ) : (
-    {
-      "all":"All",
-      "images":"Images",
-      "news":"News",
-      "videos":"Vídeos"
-    }
-  )
+
+  const navLinkTranslation =
+    lang === "Portuguese"
+      ? {
+          all: "Todos",
+          images: "Imagens",
+          news: "Notícias",
+          videos: "Vídeos",
+        }
+      : {
+          all: "All",
+          images: "Images",
+          news: "News",
+          videos: "Vídeos",
+        };
   const links = [
     { url: "/results", text: navLinkTranslation.all },
     { url: "/images", text: navLinkTranslation.images },
     { url: "/news", text: navLinkTranslation.news },
-    { url: "/videos", text: navLinkTranslation.videos }
   ];
-
 
   var pageResultsArray = [];
 
   const getSearchResults = async () => {
     setIsLoading(true);
     setNumberOfImages(4);
-    const response = await api.get(`/search/q=${searchText}&num=20`);
+    var search;
+    currentLocation === '/videos' ? search = "videos" : '' 
+    const response = await api.get(`/search/q=${searchText}${search}&num=20`);
     setSearchData([response.data]);
     setIsLoading(false);
   };
-
+  
+  
   const getImageResults = async () => {
     if (currentLocation === "/images") {
       setIsLoading(true);
       setNumberOfImages(30);
     }
-    const response = await api.get(`/images/q=${searchText}&num=${numberOfImages}`);
+    const response = await api.get(
+      `/images/q=${searchText}&num=${numberOfImages}`
+      );
     setImageData(response.data.image_results);
     setIsLoading(false);
   };
 
+  const getNewsResults = async () => {
+    const response = await api.get(`/news/q=${searchText}&num=20`);
+    setNewsData(response.data.entries);
+  };
+  
   useEffect(() => {
     getSearchResults();
     getImageResults();
+    getNewsResults();
   }, [searchText, currentLocation, numberOfImages]);
-
+  
   searchData.map((results) => {
     pageResultsArray = results.results;
   });
 
+  
   if (isLoading) return <Loading />;
   return (
     <div>
@@ -78,15 +90,18 @@ const Results = () => {
             {links.map(({ url, text }) => (
               <NavLink
                 to={url}
-                activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2"
               >
                 <div className="flex flex-row items-center gap-x-1">
                   {text === navLinkTranslation.all ? (
                     <FiSearch />
                   ) : text === navLinkTranslation.images ? (
                     <FiCamera />
-                  ) : null}
-                  {text}
+                  ) : text === navLinkTranslation.news ? (
+                    <FiFileText />
+                  ) : (
+                    <FiYoutube />
+                  )}
+                  <p className="delay-150 hover:text-blue-700">{text}</p>
                 </div>
               </NavLink>
             ))}
@@ -165,7 +180,52 @@ const Results = () => {
               </a>
             ))}
           </div>
-        ) : null}
+        ) : currentLocation === "/news" ? (
+          <div className="flex flex-wrap sm:my-5 justify-center flex-col ">
+            {newsData.map(({ id, links, source, title }) => (
+              <div
+                key={id}
+                className="md:w-screen mb-4 flex justify-center items-center"
+              >
+                <div className="rounded border bg-white dark:bg-black p-3 md:w-1/2 sm:w-80 md:mr-10">
+                  <a
+                    href={links?.[0].href}
+                    target="_blank"
+                    rel="noreferrer "
+                    className="hover:underline "
+                  >
+                    <p className="text-lg dark:text-blue-300 text-blue-700">
+                      {title}
+                    </p>
+                  </a>
+                  <div className="flex gap-4">
+                    <a
+                      href={source?.href}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="hover:underline hover:text-blue-300"
+                    >
+                      {" "}
+                      {source?.href}
+                    </a>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : 
+          <div className="flex justify-center align-center flex-wrap">
+            {pageResultsArray.map((links, index) => {
+            var ytb;
+            if (links.additional_links?.[0].href.includes("youtube")) ytb = links.additional_links?.[0].href
+            return(
+                <div key={index} className="p-2">
+                <ReactPlayer url={ytb} controls width="355px" height="200px" />
+              </div>
+              )
+            })}
+          </div>
+        }
       </div>
     </div>
   );
